566b300bbb3337095a1f2ecced855e863b906cdf
Bamis <bamisportal@gmail.com>
2025-06-25 16:49:29 +0200

    feat: scraped data

--- src/app/map-section/collectible/tooltip/tooltip.ts
+++ src/app/map-section/collectible/tooltip/tooltip.ts
@@ -1,8 +1,9 @@
-import { Component, inject, input } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { ChecklistModel, CollectibleType } from '../../../core/models';
 import { DataService } from '../../../core/services';
 import { TooltipService } from '../../../core/services/tooltip.service';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'mkworld-tooltip',
@@ -13,11 +14,18 @@ import { TooltipService } from '../../../core/services/tooltip.service';
 export class Tooltip {
   private readonly dataService = inject(DataService);
   private readonly tooltipService = inject(TooltipService);
+  private readonly domSanitizer = inject(DomSanitizer);
 
   readonly checklistModel = input.required<ChecklistModel>();
   readonly position = input<'above' | 'below' | 'left' | 'right'>('above');
 
   readonly CollectibleType = CollectibleType;
+
+  readonly youtubeLink = computed(() => {
+    return this.domSanitizer.bypassSecurityTrustResourceUrl(
+      `https://www.youtube-nocookie.com/embed/${this.checklistModel().collectibleModel!.youtubeLink}`
+    );
+  });
 
   onChecked() {
     this.dataService.updateChecklistModelChecked(this.checklistModel());
--- src/app/sticker/sticker.ts
+++ src/app/sticker/sticker.ts
@@ -1,11 +1,13 @@
 import { Component, effect, inject, input, output, signal } from '@angular/core';
 import { ChecklistModel } from '../core/models';
 import { DataService } from '../core/services';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'mkworld-sticker',
   templateUrl: './sticker.html',
   styleUrls: ['./sticker.css'],
+  imports: [TranslateModule],
 })
 export class Sticker {
   private readonly dataService = inject(DataService);
