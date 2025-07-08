57ce6f7f589f312cc572d16e4b21446e0e545364
Bamis <bamisportal@gmail.com>
2025-07-08 20:52:10 +0200

    feat: header and help

--- src/app/core/services/import-export.service.ts
+++ src/app/core/services/import-export.service.ts
@@ -50,7 +50,7 @@ export class ImportExportService {
     const url = URL.createObjectURL(blob);
     const link = document.createElement('a');
     link.href = url;
-    link.download = `mkworld-save-${new Date().toISOString().split('T')[0]}.json`;
+    link.download = `mktools-save-${new Date().toISOString().split('T')[0]}.json`;
     document.body.appendChild(link);
 
     link.click();
--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,14 +1,65 @@
 import { NgOptimizedImage } from '@angular/common';
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
-import { TranslateModule } from '@ngx-translate/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  inject,
+  input,
+  signal,
+  viewChild,
+} from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { ImportExportService } from '../core/services/import-export.service';
+import { Help } from '../help/help';
 
 @Component({
   selector: 'mkworld-header',
   templateUrl: './header.html',
   styleUrls: ['./header.css'],
-  imports: [TranslateModule, NgOptimizedImage],
+  imports: [TranslateModule, NgOptimizedImage, Help],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Header {
+  private readonly importExportService = inject(ImportExportService);
+  private readonly translateService = inject(TranslateService);
+
   readonly isMobile = input.required<boolean>();
+
+  fileInput = viewChild<ElementRef<HTMLInputElement>>('fileInput');
+
+  isHelpOpen = signal(false);
+
+  downloadSaveFile(): void {
+    this.importExportService.exportSaveFile();
+  }
+
+  uploadSaveFile(): void {
+    this.fileInput()?.nativeElement.click();
+  }
+
+  onFileSelected(event: Event): void {
+    const file = (event.target as HTMLInputElement).files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = e => {
+        try {
+          const saveFile = JSON.parse(e.target?.result as string);
+          this.importExportService.importSaveFile(saveFile);
+        } catch (error: unknown) {
+          console.error(error);
+          alert(this.translateService.instant('HEADER.ERROR_PARSING_SAVE_FILE'));
+        }
+      };
+      reader.readAsText(file);
+    }
+    (event.target as HTMLInputElement).value = '';
+  }
+
+  openHelp(): void {
+    this.isHelpOpen.set(true);
+  }
+
+  closeHelp(): void {
+    this.isHelpOpen.set(false);
+  }
 }
